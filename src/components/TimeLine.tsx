22436977a29284a6e5511f1d67251f04fd97a102
zthaver <zainthaver262@gmail.com>
2022-06-19 20:02:28 -0700

    fixed css for card component

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -8,66 +8,51 @@ const TimeLine = () =>
 {
     const timeLineData = [
         {
-            text: 'Started working on the app-ideas repository',
-            date: 'February 25 2019',
+            text: 'Started Computer Programming at Seneca College',
+            date: 'September 7 2018',
             category: {
-                tag: 'app-ideas',
+                tag: 'School',
+                color: '#FF0000'
+            },
+        },
+        {
+            text: 'Started Service Desk Analyst Co-op at Seneca College',
+            date: 'January 2 2020',
+            category: {
+                tag: 'Work',
                 color: '#FFDB14'
             },
-            link: {
-                url: 'https://github.com/florinpop17/app-ideas',
-                text: 'Check it out on GitHub'
-            }
         },
         {
-            text: 'Started the Weekly Coding Challenge program',
-            date: 'March 04 2019',
+            text: 'Started Research Assitant Co-op at Seneca College ',
+            date: 'September 2020',
             category: {
-                tag: 'blog',
-                color: '#e17b77'
+                tag: 'Work',
+                color: '#FFDB14'
             },
-            link: {
-                url: 'https://florin-pop/blog/2019/03/weekly-coding-challenge/',
-                text: 'Check it out here'
-            }
-        },
-        {
-            text: 'Got 1.000 followers on Twitter',
-            date: 'March 07 2019',
-            category: {
-                tag: 'twitter',
-                color: '#1DA1F2'
-            },
-            link: {
-                url: 'https://twitter.com/florinpop1705',
-                text: 'See profile'
-            }
+
         },
         {
             text:
-                'I published my first article in the FreeCodeCamp Medium Publication',
-            date: 'March 18 2019',
+                'Finished my Enrollment System Project ',
+            date: 'August 8 2021',
             category: {
-                tag: 'medium',
+                tag: 'project',
                 color: '#018f69'
             },
             link: {
                 url:
-                    'https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34',
+                    'https://github.com/zthaver/Enrollment-System',
                 text: 'Check it out here'
             }
         },
         {
-            text: 'Over 12.000 views in a single day on my Medium posts',
-            date: 'April 05 2019',
+            text: 'Graduated From Seneca College',
+            date: 'September 6 2021',
             category: {
-                tag: 'medium',
-                color: '#018f69'
+                tag: 'school',
+                color: '#FF0000'
             },
-            link: {
-                url: 'https://medium.com/@popflorin1705',
-                text: 'See profile'
-            }
         }
     ]
     return(
